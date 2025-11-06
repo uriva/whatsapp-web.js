ae95bd82403989bf04c6666261158adcca803359
Uri <uri.valevski@gmail.com>
2025-11-06 12:06:40 +0200

    fix-exports

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 'use strict';
 
-const Constants = require('./src/util/Constants');
+const Constants = require("./src/util/Constants.js");
 
 module.exports = {
-    Client: require('./src/Client'),
+    Client: require("./src/Client.js"),
     
     version: require('./package.json').version,
 
     // Structures
-    Chat: require('./src/structures/Chat'),
-    PrivateChat: require('./src/structures/PrivateChat'),
-    GroupChat: require('./src/structures/GroupChat'),
-    Channel: require('./src/structures/Channel'),
-    Message: require('./src/structures/Message'),
-    MessageMedia: require('./src/structures/MessageMedia'),
-    Contact: require('./src/structures/Contact'),
-    PrivateContact: require('./src/structures/PrivateContact'),
-    BusinessContact: require('./src/structures/BusinessContact'),
-    ClientInfo: require('./src/structures/ClientInfo'),
-    Location: require('./src/structures/Location'),
-    Poll: require('./src/structures/Poll'),
-    ScheduledEvent: require('./src/structures/ScheduledEvent'),
-    ProductMetadata: require('./src/structures/ProductMetadata'),
-    List: require('./src/structures/List'),
-    Buttons: require('./src/structures/Buttons'),
-    Broadcast: require('./src/structures/Broadcast'),
+    Chat: require("./src/structures/Chat.js"),
+    PrivateChat: require("./src/structures/PrivateChat.js"),
+    GroupChat: require("./src/structures/GroupChat.js"),
+    Channel: require("./src/structures/Channel.js"),
+    Message: require("./src/structures/Message.js"),
+    MessageMedia: require("./src/structures/MessageMedia.js"),
+    Contact: require("./src/structures/Contact.js"),
+    PrivateContact: require("./src/structures/PrivateContact.js"),
+    BusinessContact: require("./src/structures/BusinessContact.js"),
+    ClientInfo: require("./src/structures/ClientInfo.js"),
+    Location: require("./src/structures/Location.js"),
+    Poll: require("./src/structures/Poll.js"),
+    ScheduledEvent: require("./src/structures/ScheduledEvent.js"),
+    ProductMetadata: require("./src/structures/ProductMetadata.js"),
+    List: require("./src/structures/List.js"),
+    Buttons: require("./src/structures/Buttons.js"),
+    Broadcast: require("./src/structures/Broadcast.js"),
     
     // Auth Strategies
-    NoAuth: require('./src/authStrategies/NoAuth'),
-    LocalAuth: require('./src/authStrategies/LocalAuth'),
-    RemoteAuth: require('./src/authStrategies/RemoteAuth'),
+    NoAuth: require("./src/authStrategies/NoAuth.js"),
+    LocalAuth: require("./src/authStrategies/LocalAuth.js"),
+    RemoteAuth: require("./src/authStrategies/RemoteAuth.js"),
     
     ...Constants
 };
